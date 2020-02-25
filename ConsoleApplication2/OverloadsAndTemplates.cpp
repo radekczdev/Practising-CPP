a45e6b932da0cdee42387174f1752b706a32d71f
radekczdev <radekcz.dev@gmail.com>
2020-02-25 15:12:47 +0100

    Added tests for Templates, added Template implementation.

--- ConsoleApplication2/OverloadsAndTemplates.cpp
+++ ConsoleApplication2/OverloadsAndTemplates.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 using namespace std;
 
 // Overloading functions
@@ -11,35 +12,19 @@ double operation(double a, double b) {
 	return a / b;
 }
 
-
-// Templates
-//template <class U>
-//bool isGreaterThanZero(U number, string &result) {
-//	bool isGreater = number > 0;// ? true : false;
-//	switch (isGreater) {
-//		result += number;
-//	case true:
-//		result += " is greater than 0";
-//		break;
-//	case false:
-//		result = number + " is not greater than 0";
-//	}
-//	return isGreater;
-//}
-
-//int main() {
-
-	
-
-	//cout << "result of operation on ints: " << operation(x, y) << endl;
-	//cout << "result of operation on doubles: " << operation(a, b) << endl;
-
-	//string result = "";
-
-	//cout << "testing template 1: " << isGreaterThanZero(y, result) << endl;
-	//cout << "testing template 2: " << isGreaterThanZero(b, result) << endl;
-	//cout << "testing template 3: " << isGreaterThanZero(x, result) << endl;
-	//cout << "testing template 4: " << isGreaterThanZero(a, result) << endl;
-
-//	return 0;
-//}
+template <class U>
+bool isGreaterThanZero(U number, string &result) {
+	stringstream stringStream;
+	stringStream.precision(2);
+	stringStream << number;
+	bool isGreater = number > 0 ? true : false;
+	switch (isGreater) {
+		case true:
+			stringStream << " is greater than 0";
+			break;
+		case false:
+			stringStream << " is not greater than 0";
+	}
+	result = stringStream.str();
+	return isGreater;
+}
--- Sample-Test2/OverloadsAndTemplatesTests.cpp
+++ Sample-Test2/OverloadsAndTemplatesTests.cpp
@@ -11,3 +11,35 @@ TEST(ShouldUseOverloadForDoubles, extractDoubles) {
 	double a = 2.3, b = -2.0;
 	EXPECT_EQ(operation(a, b), 2.3 / -2.0);
 }
+
+TEST(ShouldUseTemplates, IntIsGreaterThanZero) {
+	int number = 1;
+	string expectedResult = "1 is greater than 0";
+	string result = "";
+	isGreaterThanZero(number, result);
+	ASSERT_EQ(expectedResult, result);
+}
+
+TEST(ShouldUseTemplates, IntIsLowerThanZero) {
+	int number = -5;
+	string expectedResult = "-5 is not greater than 0";
+	string result = "";
+	isGreaterThanZero(number, result);
+	ASSERT_EQ(expectedResult, result);
+}
+
+TEST(ShouldUseTemplates, DoubleIsGreaterThanZero) {
+	double number = 1.5;
+	string expectedResult = "1.5 is greater than 0";
+	string result = "";
+	isGreaterThanZero(number, result);
+	ASSERT_EQ(expectedResult, result);
+}
+
+TEST(ShouldUseTemplates, DoubleIsNotGreaterThanZero) {
+	double number = -5.2922;
+	string expectedResult = "-5.3 is not greater than 0";
+	string result = "";
+	isGreaterThanZero(number, result);
+	ASSERT_EQ(expectedResult, result);
+}
